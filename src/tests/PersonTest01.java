c8364a009b0a95322541575c5e1f6bc2c1e60dcf
Michel <rhawanjs@gmail.com>
2026-05-12 21:02:45 -0300

    update PersonTest01 to use constructor for Person instantiation

--- src/tests/PersonTest01.java
+++ src/tests/PersonTest01.java
@@ -4,9 +4,8 @@ import dominio.Person;
 
 public class PersonTest01 {
    public static void main(String[]args){
-       Person pessoa1 = new Person();
-       pessoa1.setNome("Maria");
-       pessoa1.setIdade(22);
+       Person pessoa1 = new Person("MICHEL", 19,29.90);
+
        pessoa1.imprime();
 
     }
