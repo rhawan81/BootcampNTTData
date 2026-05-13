73f5a1b29ee9f4a9d51a7dff05f6ecbcfca04267
Michel <rhawanjs@gmail.com>
2026-05-12 22:43:42 -0300

    Exercicios do Bootcamp

---
+++ src/EstruturasdControleJava/Exercicio.java
@@ -0,0 +1,11 @@
+package EstruturasdControleJava;
+
+
+
+public class Exercicio {
+    public static void main(String[]args) {
+        Tabuada numero = new Tabuada();
+        numero.exibir(4);
+
+    }
+}
---
+++ src/EstruturasdControleJava/Tabuada.java
@@ -0,0 +1,11 @@
+package EstruturasdControleJava;
+
+public class Tabuada {
+
+    public void exibir(int num){
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+
+        }
+    }
+}
