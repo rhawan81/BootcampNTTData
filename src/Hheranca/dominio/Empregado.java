ab5c10776f9cdd400fab5da497aecf9188b06684
Michel <rhawanjs@gmail.com>
2026-05-16 16:22:01 -0300

    Add Empregado class with basic attributes and methods

---
+++ src/Hheranca/dominio/Empregado.java
@@ -0,0 +1,49 @@
+package Hheranca.dominio;
+
+public class Empregado {
+    private String name;
+    private String code;
+    private String address;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
