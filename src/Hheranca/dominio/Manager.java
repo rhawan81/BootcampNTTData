c4a2c1eaffd994255f48f68133533c0f29f54a04
Michel <rhawanjs@gmail.com>
2026-05-16 16:41:13 -0300

    Add getters and setters for login, password, and commission in Manager class; override exibir method to include new attributes

--- src/Hheranca/dominio/Manager.java
+++ src/Hheranca/dominio/Manager.java
@@ -6,9 +6,37 @@ public class Manager extends Empregado {
     private String password;
     private double comission;
 
+    public String getLogin() {
+        return login;
+    }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getComission() {
+        return comission;
+    }
+
+    public void setComission(double comission) {
+        this.comission = comission;
+    }
+
+    @Override
+    public void exibir(){
+        super.exibir();
+        System.out.println("Login: " + this.login);
+        System.out.println("Password: " + this.password);
+        System.out.println("Comissão: " + this.comission);
+    }
 
 
 
