7bb7597fd3cf80d9eb0516586b7e81acdf63782f
Michel <rhawanjs@gmail.com>
2026-05-19 18:58:21 -0300

    Add Livro class and initial test implementation

---
+++ src/Hheranca/dominio/Livro.java
@@ -0,0 +1,15 @@
+package Hheranca.dominio;
+
+public class Livro {
+
+    private String nome;
+
+
+
+    public String getNome(){
+        return this.nome;
+    }
+
+
+
+}
---
+++ src/Hheranca/tests/LivroTest01.java
@@ -0,0 +1,30 @@
+package Hheranca.tests;
+
+import Hheranca.dominio.Livro;
+
+public class LivroTest01 {
+    public static void main() {
+        Livro b = new Livro();
+        Livro c = new Livro();
+        b = c;
+
+        c = null;
+        System.out.println(b);
+        System.out.println(c);
+        int [] nums;
+        nums = new int [7];
+        nums[0] = 6;
+        nums[1] = 19;
+        nums[2] = 19;
+        nums[3] = 19;
+        nums[4] = 19;
+        nums[5] = 19;
+        nums[6] = 19;
+
+
+    }
+
+
+
+
+}
