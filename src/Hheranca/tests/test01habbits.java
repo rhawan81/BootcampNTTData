53f2aaa3cb5f764ddce199044da1072bb681e879
Michel <rhawanjs@gmail.com>
2026-05-19 20:49:31 -0300

    Add test01habbits class for testing array output

---
+++ src/Hheranca/tests/test01habbits.java
@@ -0,0 +1,38 @@
+package Hheranca.tests;
+
+public class test01habbits {
+
+
+    public static void main(String[]args){
+        int [] nums = new int [5];
+        nums[0] = 10;
+        nums[1] = 20;
+        nums[2] = 30;
+        nums[3] = 40;
+        nums[4] = 50;
+
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+
+        }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
