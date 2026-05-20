933055413bb605b8858cfd20362f4cd3d1659799
Michel <rhawanjs@gmail.com>
2026-05-19 22:40:30 -0300

    Add Client class with documento attribute and getter/setter methods

---
+++ src/Hheranca/dominio/Client.java
@@ -0,0 +1,22 @@
+package Hheranca.dominio;
+
+public class Client {
+    private String documento;
+
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+
+
+
+
+
+
+
+}
