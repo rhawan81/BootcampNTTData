ef3530646d805124eb15d50585eebde86b8737ca
Michel <rhawanjs@gmail.com>
2026-05-19 22:40:51 -0300

    Add GerenteTes01 class for testing Gerente functionality

---
+++ src/Hheranca/tests/GerenteTes01.java
@@ -0,0 +1,18 @@
+package Hheranca.tests;
+
+import Hheranca.dominio.Gerente;
+
+public class GerenteTes01 {
+    public static void main(String[]args){
+
+
+        Gerente gerente = new Gerente();
+        System.out.println(gerente.isAdm());
+
+
+
+
+
+
+    }
+}
