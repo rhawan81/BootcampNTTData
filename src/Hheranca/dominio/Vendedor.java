7ab9dcf45dba2923aece5a98d3738354eee10d61
Michel <rhawanjs@gmail.com>
2026-05-19 22:41:06 -0300

    Add Vendedor class for sales management with sales tracking and user authentication methods

---
+++ src/Hheranca/dominio/Vendedor.java
@@ -0,0 +1,51 @@
+package Hheranca.dominio;
+
+public class Vendedor  extends Users{
+    private int qntdVendas;
+
+
+    public int realizarVendas(){
+
+        this.qntdVendas += 1;
+
+        return qntdVendas;
+    }
+
+
+    public int consultarVendas(){
+
+        return qntdVendas;
+
+    }
+    public int getQntdVendas() {
+        return qntdVendas;
+    }
+
+    public void setQntdVendas(int qntdVendas) {
+        this.qntdVendas = qntdVendas;
+    }
+
+
+    public boolean realizarLogin(String email , String senha){
+        return super.realizarLogin(email, senha);
+    }
+
+    public boolean realizarLogoff(String email , String senha){
+        return super.realizarLogoff(email, senha);
+
+    }
+
+    public String alterarSenha(String senha){
+        super.alterarSenha(senha);
+        return "Senha alterada com sucesso!";
+    }
+
+    public String alterarDados(String nome, String email , String senha){
+        super.alterarDados(nome, email, senha);
+        return "Dados alterados com sucesso!";
+    }
+
+
+
+
+}
