68d585edcd62d32eb9220a68d0f1fa1cf0f61371
Michel <rhawanjs@gmail.com>
2026-05-20 19:45:26 -0300

    feat: exercicio sistema de usuarios - NTT Data Bootcamp

--- src/Hheranca/tests/GerenteTes01.java
+++ src/Hheranca/tests/GerenteTes01.java
@@ -1,15 +1,29 @@
 package Hheranca.tests;
 
+import Hheranca.dominio.Atendente;
 import Hheranca.dominio.Gerente;
+import Hheranca.dominio.Vendedor;
 
 public class GerenteTes01 {
     public static void main(String[]args){
-
-
         Gerente gerente = new Gerente();
-        System.out.println(gerente.isAdm());
+        Vendedor vendedor = new Vendedor();
+        Atendente atendente = new Atendente();
+       vendedor.setNome("João");
+       vendedor.setEmail("rhawan@gmail.com");
+       vendedor.setSenha("1");
+       vendedor.realizarLogin("rhawan@gmail.com","1");
+        vendedor.realizarVendas();
+        vendedor.realizarVendas();
+        vendedor.realizarVendas();
+
+        atendente.receberPagamento(150);
+        atendente.receberPagamento(200);
 
 
+
+        gerente.gerarRelatorioFinanceiro(vendedor, atendente);
+        gerente.consultarVendas(vendedor, atendente);
 
 
 
