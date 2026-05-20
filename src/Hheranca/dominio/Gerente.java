81838255d896c7ef5e38c38f660d1a7b19384378
Michel <rhawanjs@gmail.com>
2026-05-20 19:45:16 -0300

    feat: exercicio sistema de usuarios - NTT Data Bootcamp

--- src/Hheranca/dominio/Gerente.java
+++ src/Hheranca/dominio/Gerente.java
@@ -8,11 +8,15 @@ public class Gerente extends Users{
 
 
     public boolean realizarLogin(String email , String senha){
-        return super.realizarLogin(email, senha);
+        super.realizarLogin(email, senha);
+
+        return true;
     }
 
     public boolean realizarLogoff(String email , String senha){
-        return super.realizarLogoff(email, senha);
+         super.realizarLogoff(email, senha);
+
+        return true;
 
         }
 
@@ -24,6 +28,24 @@ public class Gerente extends Users{
     public String alterarDados(String nome, String email , String senha){
        super.alterarDados(nome, email, senha);
         return "Dados alterados com sucesso!";
+    }
+
+
+
+    public void gerarRelatorioFinanceiro(Vendedor vendedor , Atendente atendente){
+
+        System.out.println("====== Relatório Financeiro ========");
+        System.out.println("Vendas realizadas pelo vendedor: " + vendedor.consultarVendas());
+        System.out.println("Valor total em caixa do atendente: " + atendente.getValorEmCaixa());
+
+        System.out.println("Relatório gerado com sucesso!");
+
+
+    }
+    public  void consultarVendas(Vendedor vendedor , Atendente atendente){
+        System.out.println("Gerente consultando vendas...");
+        System.out.println("Vendas realizadas: " + vendedor.consultarVendas());
+        System.out.println("Vendas em caixa do  atendente : " + atendente.getValorEmCaixa());
     }
 
 
